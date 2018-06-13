f10564cf8f687c6052a6f9d277617654b129cb39
YWHo <YWHo@users.noreply.github.com>
2018-06-13 13:54:07 +1200

    add primality_test

---
+++ primality_test.js
@@ -0,0 +1,41 @@
+const readline = require('readline')
+
+/* Given a value, test whether the given number is prime number 
+
+The solution is from https://stackoverflow.com/questions/1801391/what-is-the-best-algorithm-for-checking-if-a-number-is-prime, by saurabh kumar
+
+*/
+
+
+function isPrime (num) {
+  if (num <= 1) return false
+  if (num <= 3) return true
+  if (num%2 == 0 || num%3 == 0) return false;
+
+  for (let i=5; i*i<=num; i+=6) {
+    if (num%i == 0 || num%(i+2) == 0) {
+      return false
+    }
+  }
+
+  return true;
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter a number:  ', (answer) => {
+
+  if (isPrime(answer)) {
+    console.log(answer + " is a prime number")
+  } else {
+    console.log(answer + " is not a prime number")
+  }
+
+  rl.close();
+});
+
+
+
